00eb3f00d8ac59f6b6d1c6fb7fce05e19198dcd6
Leonidas Pastras <pastras@thinkdesquared.com>
2025-01-09 20:26:02 +0200

    Add basic Firebase configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,6 +51,9 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.firebase.bom))      // Import BOM
+    implementation(libs.firebase.firestore)          // Firestore
+    implementation(libs.firebase.analytics)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
