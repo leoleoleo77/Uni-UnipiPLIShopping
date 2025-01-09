0b1f949eb0780451d8b1012b57b77181ec1652c2
Leonidas Pastras <pastras@thinkdesquared.com>
2025-01-09 19:51:09 +0200

    increase compileSdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.leo.unipiplishopping"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.leo.unipiplishopping"
